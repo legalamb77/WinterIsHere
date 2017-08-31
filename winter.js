b9631b85a5a85e01a7fb53c8ebd39c74983ed76e
Ted Morley <theodore.morley@mail.mcgill.ca>
2017-08-31 00:04:25 +0000

    Working prototype, now displays images where you click and plays the sound

--- winter.js
+++ winter.js
@@ -6,6 +6,13 @@ function winterNow(event){
     //play sound using howler
     sound.play();
     //display image where the click is
+    var newImage = document.createElement("img");
+    newImage.setAttribute('src', 'assets/Coldwraith.png');
+    newImage.setAttribute('class', 'overlays');
+    newImage.style.left = event.clientX + "px";
+    newImage.style.top = event.clientY + "px";
+    console.log(event.clientX);
+    document.body.appendChild(newImage);
 }
 
 document.addEventListener('click', winterNow);
