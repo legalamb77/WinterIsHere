195f752220b8311207432430073baf0fe2f084fe
Ted Morley <theodore.morley@mail.mcgill.ca>
2017-08-31 01:23:12 +0000

    Added golden card!

--- winter.js
+++ winter.js
@@ -1,13 +1,23 @@
-var sound = new Howl({
+var soundNormal = new Howl({
   src: ['assets/winterishere.mp3']
+});
+var soundGold = new Howl({
+  src: ['assets/AUGH.mp3']
 });
 
 function winterNow(event){
-    //play sound using howler
-    sound.play();
     //display image where the click is
     var newImage = document.createElement("img");
-    newImage.setAttribute('src', 'assets/Coldwraith.png');
+    var goldOrNormal = Math.random();
+    if(goldOrNormal>0.1){
+      newImage.setAttribute('src', 'assets/Coldwraith.png');
+      //play sound using howler
+      soundNormal.play();
+    }
+    else{
+      newImage.setAttribute('src', 'assets/goldenCold.gif');
+      soundGold.play();
+    }
     newImage.setAttribute('class', 'overlays');
     var x=event.clientX-100;
     var y=event.clientY-100;
